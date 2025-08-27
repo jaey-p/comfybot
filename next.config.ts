cf817beb24404aaeecc17d49869c62d93178d72e
Jay P <jay@sorbet.studio>
2025-08-28 01:22:32 +0530

    fix: add security headers (experimental)

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,44 @@ export default () => {
     experimental: {
       taint: true,
     },
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "X-Content-Type-Options",
+              value: "nosniff",
+            },
+            {
+              key: "X-Frame-Options",
+              value: "DENY",
+            },
+            {
+              key: "Referrer-Policy",
+              value: "strict-origin-when-cross-origin",
+            },
+          ],
+        },
+        {
+          source: "/sw.js",
+          headers: [
+            {
+              key: "Content-Type",
+              value: "application/javascript; charset=utf-8",
+            },
+            {
+              key: "Cache-Control",
+              value: "no-cache, no-store, must-revalidate",
+            },
+            {
+              key: "Content-Security-Policy",
+              value: "default-src 'self'; script-src 'self'",
+            },
+          ],
+        },
+      ];
+    },
   };
   const withNextIntl = createNextIntlPlugin();
   return withNextIntl(nextConfig);
